4996618c7fa20bbcca43372e370f9476f613a5c5
Ethan <uhhhhhethan@gmail.com>
2024-01-04 15:57:40 -0800

    invoke function

--- app.js
+++ app.js
@@ -20,10 +20,13 @@ var userNamePrompt = prompt('what\'s your username?');
 var passwordPrompt = prompt('what\'s your password?');
 
 function signIn(user, password) {
-    if (user === databse[0].username && 
-        password === databse[0].password) {
+    if (user === database[0].username && 
+        password === database[0].password) {
             console.log(newsFeed);
+            console.log('you are signed in');
         } else {
             alert('sorry, wrong username and password');
         }
 }
+
+signIn(userNamePrompt, passwordPrompt);
