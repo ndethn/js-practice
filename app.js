19911dfb330c4b7f27d6f6308b2ed4621798e943
Ethan <uhhhhhethan@gmail.com>
2024-01-04 15:49:19 -0800

    declare arrays and objects

---
+++ app.js
@@ -0,0 +1,17 @@
+var database = [
+    {
+        username: 'michael',
+        password: 'whatpassword'
+    }
+];
+
+var newsFeed = [
+    {
+        username: 'max',
+        timeline: 'so tired from learning javascript',
+    },
+    {
+        username: 'mary',
+        timeline: 'javascript is the best'
+    }
+];
